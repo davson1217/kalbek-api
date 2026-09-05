cb8ef72a66be4744f7f52c926cf979ed771f2e10
David Olurebi <olurebidavid@gmail.com>
2026-09-05 14:48:20 +0300

    configure cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -15,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(HandleCors::class);
+
         $middleware->alias([
             'cms' => EnsureCmsAccess::class,
         ]);
